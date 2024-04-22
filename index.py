f71205533e787fc653059334109af270155a7f0d
marcelolinck <malveslinck@gmail.com>
2024-04-21 23:19:42 -0400

    Ajustado teste de saida.

---
+++ index.py
@@ -0,0 +1,30 @@
+import serial
+import time
+
+# Configurações da porta serial
+porta_serial = 'COM4'  # Porta serial no Windows
+baud_rate = 9600
+
+# Inicializa a comunicação serial
+arduino = serial.Serial(porta_serial, baud_rate)
+time.sleep(2)  # Aguarda a inicialização da porta serial
+
+while True:
+    # Solicita ao usuário que digite uma opção
+    opcao = input("Digite 'vidro', 'lata', 'plastico' ou 'x' para sair: ").strip().lower()
+
+    # Verifica se o usuário deseja sair
+    if opcao == 'x':
+        print("Saindo...")
+        arduino.write(opcao.encode())  # Envie 'x' para o Arduino
+        time.sleep(5)
+        break
+
+    # Verifica se a opção é válida e envia para o Arduino
+    if opcao in ['vidro', 'lata', 'plastico']:
+        arduino.write(opcao.encode())  # Envie a opção para o Arduino
+    else:
+        print("Opção inválida!")
+
+# Fecha a comunicação serial
+arduino.close()
